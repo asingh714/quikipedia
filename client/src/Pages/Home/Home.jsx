1adc1b2ee06ff6df1bf32c652e1f10be6d92b426
Aman <amsingh714@gmail.com>
2024-01-13 16:07:17 -0500

    styled a floating background for the main pge

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,14 +1,21 @@
 import "./Home.css";
+import Search from "../../assets/search.svg";
 
 const Home = () => {
   return (
-    <main>
+    <main className="main-container">
+      <div className="bg-container">
+        <div className="bg-shape bg-shape1"></div>
+        <div className="bg-shape bg-shape2"></div>
+        <div className="bg-shape bg-shape3"></div>
+      </div>
       <header>
         <h1>Quikipedia</h1>
         <p>Wikipedia summaries for those in a hurry</p>
       </header>
       <form>
-        <input type="text" className="main-input"/>
+        <input type="text" className="main-input" />
+        <img src={Search} alt="" />
         <div>
           <label htmlFor="funModeSwitch" className="switch-label">
             Fun Mode
